65dc10c582fa959c8f91f6dd27d436e06c6329d2
Vitor Fernandes dos Santos <vitorfernandes11122006@gmail.com>
2026-09-03 23:20:57 -0300

    "new files"

--- test.py
+++ test.py
@@ -1,6 +1,28 @@
 from scipy.stats import *
 import pandas as pd
 
+df = pd.read_csv('checkpoint_magos_dados.csv')
+df["soma_skills"] = df["precisao_pct"] + df["potencia_pct"] + df["controle_pct"] + df["dificuldade"] + df["resistencia_alvo"] + (df["sucesso"] * 10) - df["dano_colateral"]  - df["tempo_execucao_s"] 
+
+df.to_csv('1checkpoint_magos_dados.csv')
+
+namesList2 = df['mago'].tolist()
+namesList1 = df['mago'].tolist()
+
+namesList = list(dict(zip(namesList1,namesList2)).keys())
+
+dictCount = {}
+for name in namesList:
+
+    dfName = df[df['mago'] == name]
+    soma_skill = int(dfName["soma_skills"].sum())
+
+    dictCount[name] = soma_skill
+
+# print(dfName)
+# print(dictCount)
+
+df.to_excel("salvar.xlsx")
 
 def scipyTests(dataset: str, columns:list[int] = None, alpha: float=0.05, criteria:list = 0, alternative:str = None, greaterOrLess:str = None) ->None:
 
@@ -19,11 +41,13 @@ def scipyTests(dataset: str, columns:list[int] = None, alpha: float=0.05, criter
         dfGroup1 = df[df[col1] > criteria[0]]
         dfGroup2 = df[df[col1] < criteria[0]]
 
+        print(dfGroup1)
+
     elif alternative == 'str':
             dfGroup1 = df[df[col1] == criteria[0]]
             dfGroup2 = df[df[col1] == criteria[1]]
 
-    print(f'\nSimple Means (Not valid for comparison)\n \n dfGroup1 = {dfGroup1[col2].mean()} \n dfGroup2 = {dfGroup2[col2].mean()} \n')
+    # print(f'\nSimple Means (Not valid for comparison)\n \n dfGroup1 = {dfGroup1[col2].mean()} \n dfGroup2 = {dfGroup2[col2].mean()} \n')
 # ______________________________________________________________________________________________________________________________________
 
 # Set distribuition using shapiro
@@ -46,14 +70,20 @@ def scipyTests(dataset: str, columns:list[int] = None, alpha: float=0.05, criter
     if dist == 'normal':
 
         if variety == 'Homocedástica':
-            return print(f'Distribuition {dist} and Variety {variety} (Use ttest_ind) \n\nIs H0 refusable? {"Yes" if ttest_ind(dfGroup1[col2], dfGroup2[col2], alternative=greaterOrLess)[1] < alpha else "No"}')
+            # print(f'Distribuition {dist} and Variety {variety} (Use ttest_ind) \n\nIs H0 refusable? {"Yes" if ttest_ind(dfGroup1[col2], dfGroup2[col2], alternative=greaterOrLess)[1] < alpha else "No"}')
+
+            return True if mannwhitneyu(dfGroup1[col2], dfGroup2[col2], alternative=greaterOrLess)[1] < alpha else False
 
         if variety == 'Heterocedástica': 
-            return print(f'Distribuition {dist} and Variety {variety} (Use ttest_ind(equal_var=False)) \n\nIs H0 refusable? {"Yes" if ttest_ind(dfGroup1[col2], dfGroup2[col2], alternative=greaterOrLess, equal_var=False)[1] < alpha else "No"}')
+            # print(f'Distribuition {dist} and Variety {variety} (Use ttest_ind(equal_var=False)) \n\nIs H0 refusable? {"Yes" if ttest_ind(dfGroup1[col2], dfGroup2[col2], alternative=greaterOrLess, equal_var=False)[1] < alpha else "No"}')
+
+            return True if mannwhitneyu(dfGroup1[col2], dfGroup2[col2], alternative=greaterOrLess)[1] < alpha else False
 
 
     if dist == 'not normal':
-            return print(f'Distribuition {dist} and Variety {variety} (Use mannwhitneyu) \n\nIs H0 refusable? {"Yes" if mannwhitneyu(dfGroup1[col2], dfGroup2[col2], alternative=greaterOrLess)[1] < alpha else "No"}  \n')
+            # print(f'Distribuition {dist} and Variety {variety} (Use mannwhitneyu) \n\nIs H0 refusable? {"Yes" if mannwhitneyu(dfGroup1[col2], dfGroup2[col2], alternative=greaterOrLess)[1] < alpha else "No"}  \n')
+
+            return True if mannwhitneyu(dfGroup1[col2], dfGroup2[col2], alternative=greaterOrLess)[1] < alpha else False
      
 
 #_______________________________________________________________________________________________________________________________________
@@ -84,4 +114,10 @@ def scipyTests(dataset: str, columns:list[int] = None, alpha: float=0.05, criter
 
 # H0 = Pink cable doesn't have less lights blinking than Blue Cable
 # H1 = Pink cable have less lights blinking than Blue Cable
-scipyTests('dataset_wifi_alien.csv', columns=[7, 3], criteria=['rosa','azul'], alternative='str', greaterOrLess='less')
+# scipyTests('dataset_wifi_alien.csv', columns=[7, 3], criteria=['rosa','azul'], alternative='str', greaterOrLess='less')
+
+# H1 = Pink cable have less lights blinking than Blue Cable
+# H1 = Pink cable have less lights blinking than Blue Cable
+
+for name in namesList:
+    print(f"For Snape vs {name} Is H0 refusable? {scipyTests('1checkpoint_magos_dados.csv', columns=[4, 18], criteria=['Severus Snape', name], alternative='str', greaterOrLess='greater')}")
